e4afdf60500fdd89e1b45e51e901f831b1446c75
Freddie <freddie.mao@gmail.com>
2021-10-27 15:19:26 +0100

    currently working before attempting to make login for users

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 from datetime import datetime
 from functools import wraps
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import abort, default_exceptions, HTTPException, InternalServerError
@@ -25,13 +25,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-def login_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated()
+
 
 def createConnection():
     return sqlite3.connect("atlas.db")
@@ -45,7 +39,7 @@ def about():
     return render_template("about.html")
 
 @app.route("/post", methods=["GET", "POST"])
-@login_required
+
 def post():
     return render_template("post.html")
 
