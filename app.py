00b2285c06e8bcd4b9801727a16461bc793873ec
ChairmanFMao <89081250+ChairmanFMao@users.noreply.github.com>
2021-10-27 10:56:59 +0100

    Add files via upload
    Adding the python file and requirements file

---
+++ app.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import Flask, render_template, request
+from flask_session import Session
+from werkzeug.exceptions import abort, default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello there"
