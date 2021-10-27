57ab311ac003756726b7a34c38c854abceeef8f3
Freddie <freddie.mao@gmail.com>
2021-10-27 14:44:02 +0100

    added tables for users, posts and comments to atlas.db

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import sqlite3
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_session import Session
 from werkzeug.exceptions import abort, default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+def createConnection():
+    return sqlite3.connect("atlas.db") 
 
 @app.route("/")
 def index():
@@ -14,15 +18,15 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/post")
+@app.route("/post", methods=["GET", "POST"])
 def post():
     return render_template("post.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
