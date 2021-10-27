34b1622ba321ffbd94ad06503408cc6f3122f867
Freddie <freddie.mao@gmail.com>
2021-10-27 14:54:40 +0100

    added some stuff to store sessions and make sure not responses are cached

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 import sqlite3
+import os
+
 from datetime import datetime
-from flask import Flask, render_template, request
+from functools import wraps
+from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
+from tempfile import mkdtemp
 from werkzeug.exceptions import abort, default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+ 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated()
+
 def createConnection():
-    return sqlite3.connect("atlas.db") 
+    return sqlite3.connect("atlas.db")
 
 @app.route("/")
 def index():
@@ -19,6 +45,7 @@ def about():
     return render_template("about.html")
 
 @app.route("/post", methods=["GET", "POST"])
+@login_required
 def post():
     return render_template("post.html")
 
