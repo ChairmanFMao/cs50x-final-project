3f80b4c63d14cf33b4b3a3004059aded3abcf434
Freddie <freddie.mao@gmail.com>
2021-10-29 20:21:21 +0100

    sessions permanent

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def after_request(response):
 
 app.jinja_env.filters["getUsername"] = getUsername
 
-app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
